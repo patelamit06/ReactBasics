4fdb99112f9b377fc043e4164fde74fd16f289b1
amitpatel <cadminamipat@dustin.com>
2025-04-21 20:28:06 +0200

    first comit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import HomeCards from './components/HomeCards'
+import JobListings from './components/JobListings'
+
+const App = () => {
+  return (
+   <>
+      <Navbar/>
+      <Hero title = "Test Title" subtitle = "This is SubTitle 1"/>
+      <HomeCards/>
+      <JobListings/>
+ 
+
+   
+
+    <section className="m-auto max-w-lg my-10 px-6">
+      <a
+        href="jobs.html"
+        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs</a
+      >
+    </section>
+    </>
+  )
+}
+
+export default App
+
